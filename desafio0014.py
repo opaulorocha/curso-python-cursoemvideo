2a99238e9413aa79c417f31323043c3000494e45
Paulo Rocha <124544730+opaulorocha@users.noreply.github.com>
2026-03-18 12:15:20 -0300

    Delete desafio0014.py

--- desafio0014.py
+++
@@ -1,4 +0,0 @@
-# Programa de convesao de celsius para fahrenheit
-c = float(input("Qual a temperatura em graus celsius? "))
-f = c * 9/5 + 32
-print("Convertendo {} graus celsius para Fahrenheit da {:.2f}".format(c, f))
